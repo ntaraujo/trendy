5382cea4d1c66b4164c3a2459eac4490163eb7bd
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-02-02 14:51:51 -0200

    date functions

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,18 @@
+import locale
+locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
+
 def msg(text):
     print(f'#### TRENDY ---- {text}')
+
+def simple_to_datetime(date):
+    from datetime import datetime
+    return datetime(int(date[4:]), int(date[2:4]), int(date[:2]))
+
+def datetime_to_simple(date):
+    return date.strftime('%d%m%Y')
+
+def capitalized_month(date):
+    return date.strftime('%B').upper()
+
+if __name__ == '__main__':
+    print(capitalized_month(simple_to_datetime('03033445')))
