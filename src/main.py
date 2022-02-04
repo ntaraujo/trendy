9617d18f0537df4bdec76e030d559a886bb10590
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-02-04 12:08:57 -0300

    shutdown_signal ctrl_c

--- src/main.py
+++ src/main.py
@@ -1,11 +1,22 @@
 from gooey import Gooey, GooeyParser, local_resource_path
 from automators.web import Web
 from automators.excel import Excel
+import signal
 
 web = Web()
 excel = Excel()
 
-@Gooey(language="portuguese", program_name="Trendy", image_dir=local_resource_path("gooey-images"), language_dir=local_resource_path("gooey-languages"), navigation="SIDEBAR", sidebar_title="Ações", show_success_modal=False, show_sidebar=True)
+@Gooey(
+    language="portuguese",
+    program_name="Trendy",
+    image_dir=local_resource_path("gooey-images"),
+    language_dir=local_resource_path("gooey-languages"),
+    navigation="SIDEBAR",
+    sidebar_title="Ações",
+    show_success_modal=False,
+    show_sidebar=True,
+    shutdown_signal=signal.CTRL_C_EVENT
+    )
 def main():
     parser = GooeyParser(description="Aplicativo de automação com planilhas e sistemas online")
     parser.parse_args()
