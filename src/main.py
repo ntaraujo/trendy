3a2d1dc9542df7ab75d8e2c692c8612f6b43ec1c
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-02-04 12:06:34 -0300

    TABBED to SIDEBAR

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from automators.excel import Excel
 web = Web()
 excel = Excel()
 
-@Gooey(language="portuguese", program_name="Trendy", image_dir=local_resource_path("gooey-images"), language_dir=local_resource_path("gooey-languages"), navigation="TABBED", sidebar_title="Ações", show_success_modal=False, show_sidebar=True)
+@Gooey(language="portuguese", program_name="Trendy", image_dir=local_resource_path("gooey-images"), language_dir=local_resource_path("gooey-languages"), navigation="SIDEBAR", sidebar_title="Ações", show_success_modal=False, show_sidebar=True)
 def main():
     parser = GooeyParser(description="Aplicativo de automação com planilhas e sistemas online")
     parser.parse_args()
