bac7daa3f54cb67eff040a88120f1633801983da
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-02-04 12:00:18 -0300

    update directory structure and fix module in src/ not being found

--- src/options/posicao.py
+++ src/options/posicao.py
@@ -1,3 +1,8 @@
+if __name__ == '__main__':
+    from gooey import local_resource_path
+    from sys import path as sys_path
+    sys_path.insert(0, local_resource_path(""))
+
 from utils import msg, pasted_to_list
 
 class Posicao():
