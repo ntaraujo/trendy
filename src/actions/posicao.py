95a5bcfc9a6a1fde7634c098a321cd88f1e79077
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-02-08 19:16:11 -0200

    put self.

--- src/actions/posicao.py
+++ src/actions/posicao.py
@@ -13,12 +13,11 @@ class Posicao:
         self.args = args
         self.web = web
         self.excel = excel
-
-        self.cods_clientes = pasted_to_list(args['cods_cliente'] or '')
-        self.nomes_clientes = pasted_to_list(args['nomes_cliente'] or '')
+        self.cods_clientes = pasted_to_list(self.args['cods_cliente'] or '')
+        self.nomes_clientes = pasted_to_list(self.args['nomes_cliente'] or '')
 
         if not self.cods_clientes or not self.nomes_clientes:
-            for rede, nome_cliente, cod_cliente in excel.file_vertical_search(args['nome_rede'].upper(), args['dinamica'], 9,
+            for rede, nome_cliente, cod_cliente in self.excel.file_vertical_search(self.args['nome_rede'].upper(), self.args['dinamica'], 9,
                                                                               9, 10, 11):
                 if cod_cliente is None or cod_cliente == '':
                     msg(f'CLIENTE SEM CÓDIGO: "{rede}" --> "{nome_cliente}"')
@@ -46,15 +45,15 @@ class Posicao:
                 else:
                     this_month -= 1
 
-        if not web.opened:
-            web.open()
-        web.totvs_access()
-        if not web.totvs_logged:
-            password = args['senha_totvs']
-            web.totvs_login(password)
-        web.totvs_fav_pedidos()
+        if not self.web.opened:
+            self.web.open()
+        self.web.totvs_access()
+        if not self.web.totvs_logged:
+            password = self.args['senha_totvs']
+            self.web.totvs_login(password)
+        self.web.totvs_fav_pedidos()
 
-        excel.open_macros()
+        self.excel.open_macros()
 
         self.make_workbook()
 
@@ -63,15 +62,13 @@ class Posicao:
     def make_workbook(self):
         msg('Construindo a Posição da rede')
 
-        args, web, excel = self.args, self.web, self.excel
-
-        excel.open()
+        self.excel.open()
 
         for cod_cliente, nome_cliente in zip(self.cods_clientes, self.nomes_clientes):
-            excel.new_sheet(nome_cliente)
+            self.excel.new_sheet(nome_cliente)
             self.make_sheet(cod_cliente, nome_cliente)
 
-        excel.file.sheets[0].delete()
+        self.excel.file.sheets[0].delete()
 
     @staticmethod
     def filter_table(complete_table):
@@ -91,29 +88,26 @@ class Posicao:
         msg(f'Construindo a Posição da loja "{nome_cliente}"')
 
         from utils import capitalized_month, simple_to_datetime
-
-        excel = self.excel
-        web = self.web
-
-        excel.insert(nome_cliente)
-        inserted = excel.back_range(1, 9)
-        excel.bold(inserted)
-        excel.center(inserted)
-        excel.color(inserted, 255, 0, 0)
-        excel.merge_across(inserted)
+        
+        self.excel.insert(nome_cliente)
+        inserted = self.excel.back_range(1, 9)
+        self.excel.bold(inserted)
+        self.excel.center(inserted)
+        self.excel.color(inserted, 255, 0, 0)
+        self.excel.merge_across(inserted)
 
         for prev_emb in self.prevs_emb:
-            web.totvs_fav_pedidos_fill(cod_cliente, prev_emb, "01012000")
-            table = self.filter_table(web.totvs_fav_pedidos_complete_table())
+            self.web.totvs_fav_pedidos_fill(cod_cliente, prev_emb, "01012000")
+            table = self.filter_table(self.web.totvs_fav_pedidos_complete_table())
 
-            excel.insert([[None], ["PEDIDO " + capitalized_month(simple_to_datetime(prev_emb))]])
-            inserted = excel.back_range(2, 9)
-            excel.bold(inserted)
-            excel.center(inserted)
-            excel.merge_across(inserted)
-            excel.insert(table)
+            self.excel.insert([[None], ["PEDIDO " + capitalized_month(simple_to_datetime(prev_emb))]])
+            inserted = self.excel.back_range(2, 9)
+            self.excel.bold(inserted)
+            self.excel.center(inserted)
+            self.excel.merge_across(inserted)
+            self.excel.insert(table)
 
-        excel.run("posicao_general_format")
+        self.excel.run("posicao_general_format")
 
 
 if __name__ == '__main__':
