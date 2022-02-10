261cc4f1ab87d2ad552f91bf30dd9d075e70515e
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-02-10 17:16:07 -0200

    Revert "Revert "fix not displaying utf-8 characters""
    This reverts commit 691dc3bd8046f39077ad1fc51323c253b470b994.

--- src/utils.py
+++ src/utils.py
@@ -16,7 +16,6 @@ locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
 
 
 def msg(text):
-    text = text.encode('ascii', 'replace').decode()
     print(f'#### TRENDY ---- {text}')
     log_file.write(text + '\n')
 
