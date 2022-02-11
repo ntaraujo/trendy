5b3a546b836bce9415173afdef6d2bfbe1ecf276
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-02-11 14:27:26 -0200

    create .dmg for .app

--- release/main.sh
+++ release/main.sh
@@ -1,4 +1,22 @@
 cd release
 pyinstaller --noconfirm -F --windowed build-osx.spec
-open -n dist/Trendy.app
-mkdir dist/Contents
+cd dist
+
+mkdir Contents
+mkdir TrendyDMG
+
+rm -r TrendyDMG/Trendy.app
+mv Trendy.app TrendyDMG/
+open -n TrendyDMG/Trendy.app
+
+rm '../Instalador Trendy.dmg'
+create-dmg \
+  --volname "Instalador Trendy" \
+  --window-pos 200 120 \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "Trendy.app" 200 190 \
+  --hide-extension "Trendy.app" \
+  --app-drop-link 600 185 \
+  '../Instalador Trendy.dmg' \
+  TrendyDMG
