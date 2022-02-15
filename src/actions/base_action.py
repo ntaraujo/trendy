45ce5ea3c2bb44e3a08d93a5e38cb699b2583372
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-02-15 18:13:32 -0200

    new base classes ExcelAction and RedeAction

--- src/actions/base_action.py
+++ src/actions/base_action.py
@@ -1,5 +1,53 @@
+from utils import pasted_to_list, msg
+
 class BaseAction:
     def __init__(self, args, web, excel):
         self.args = args
         self.web = web
         self.excel = excel
+
+
+class ExcelAction(BaseAction):
+
+    def make_workbook(self):
+        raise NotImplementedError()
+
+    @staticmethod
+    def filter_table(complete_table):
+        raise NotImplementedError()
+
+    def make_sheet(self, cod_cliente, nome_cliente):
+        raise NotImplementedError()
+
+
+class RedeAction(ExcelAction):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cods_clientes = pasted_to_list(self.args.cods_cliente or '')
+        self.nomes_clientes = pasted_to_list(self.args.nomes_cliente or '')
+
+        if not self.cods_clientes or not self.nomes_clientes:
+            for status, rede, nome_cliente, cod_cliente in self.excel.file_vertical_search(
+                    self.args.nome_rede.upper(), self.args.dinamica, 9, 5, 9, 10, 11):
+                if cod_cliente is None or cod_cliente == '':
+                    msg(f'CLIENTE SEM CÓDIGO: "{rede}" --> "{nome_cliente}"')
+                    continue
+                elif status is None or "INATIVO" in status.upper():
+                    continue
+
+                msg(f'Cliente encontrado: "{rede}" --> "{nome_cliente}" --> "{cod_cliente}"')
+                self.cods_clientes.append(cod_cliente)
+                self.nomes_clientes.append(nome_cliente)
+
+    def make_workbook(self):
+        msg('Construindo a pasta de trabalho da rede')
+
+        self.excel.open_app()
+        self.excel.open_macros()
+        self.excel.open()
+
+        for cod_cliente, nome_cliente in zip(self.cods_clientes, self.nomes_clientes):
+            self.excel.new_sheet(nome_cliente)
+            self.make_sheet(cod_cliente, nome_cliente)
+
+        self.excel.delete_sheet(0)
