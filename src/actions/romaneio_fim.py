0fe95d99d4ed82e8631e19cc88984bead92e120c
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-02-21 13:03:16 -0300

    faster actions.romaneio_fim

--- src/actions/romaneio_fim.py
+++ src/actions/romaneio_fim.py
@@ -11,6 +11,8 @@ from actions.base_action import BaseAction
 from utils import run_scheduled, global_path
 import utils
 
+utils.running_scheduled = True
+
 
 class RomaneioFim(BaseAction):
     def __init__(self, args, web, excel):
@@ -23,27 +25,21 @@ class RomaneioFim(BaseAction):
 
         arquivo_oc_dict = {line[7].strip()+line[10].strip(): line for line in arquivo_oc[arquivo_oc_start_at:]}
 
-        utils.running_scheduled = True
-
         self.excel.open_app()
         self.excel.open(global_path(self.args.romaneio_inicio))
+        cod_refs = [cod[:10].strip() for cod in self.excel.file.sheets.active.range('C14:C100').value if cod]
 
-        sheet_range = self.excel.file.sheets.active.range
+        materials = []
+        ocs = []
 
-        for line in range(14, 101):
-            cod = sheet_range((line, 3)).value
+        for cod_ref in cod_refs:
+            o, _, m = arquivo_oc_dict[cod_ref][oc_ind:material_ind+1]
+            materials.append((m,))
+            ocs.append((o,))
 
-            if not cod:
-                break
-
-            cod_ref = cod[:10].strip()
-            material = arquivo_oc_dict[cod_ref][material_ind].strip()
-            oc = arquivo_oc_dict[cod_ref][oc_ind].strip()
-
-            self.excel.assign((line, 4), material)
-            self.excel.assign((line, 11), oc)
-        
-        self.excel.assign('G10', oc)
+        self.excel.assign('D14', materials)
+        self.excel.assign('K14', ocs)
+        self.excel.assign('G10', ocs[-1])
 
         run_scheduled()
 
